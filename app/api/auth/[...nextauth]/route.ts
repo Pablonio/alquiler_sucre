2a5bbaca9e53617b2e1c2c6852f8072c867cfff9
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-03-26 11:25:45 -0400

    CHANGESSSS

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,32 @@
-// Importaciones necesarias
+// Import NextAuth and necessary providers
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
-export const authOptions: AuthOptions = {
+// Configure authentication options
+const authOptions: AuthOptions = {
   providers: [
-    // Proveedor de Google
+    // Google provider
     GoogleProvider({
-      clientId: "490635578120-u9tp8nkpntiafp21ni6cqddq8lpuve4n.apps.googleusercontent.com" as string,
-      clientSecret: "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" as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // Proveedor de Azure AD
+    // Azure AD provider
     AzureADProvider({
-      clientId: "a6ec488f-237a-45bd-8a7b-15c450441267" as string,
-      clientSecret: "Ftu8Q~4V3zQREvjFPvgdW1GORkueUn2qfO2vCatP" as string,
-      tenantId: "aa8d4281-8fb7-4918-bb18-f85313ad7047" as string,
+      clientId: process.env.MICROSOFT_CLIENT_ID as string,
+      clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
+      tenantId: process.env.MICROSOFT_TENANT_ID as string,
     }),
   ],
   debug: process.env.NODE_ENV === 'development',
   session: {
     strategy: "jwt",
   },
-  secret: "NEXTAUTH_SECRET",
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
+// NextAuth handler
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// Export the NextAuth handler for both GET and POST requests
+export default handler;
